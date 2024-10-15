8af56333e5be01607aa79bec2d2e609ae4254eb4
Kevin Wong <kev5ntech@gmail.com>
2024-10-15 15:15:16 -0700

    Initial Commit

---
+++ DesignAnInterface/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DesignAnInterface
+//
+//  Created by Kevin Wong on 10/15/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DesignAnInterface/DesignAnInterfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  DesignAnInterfaceApp.swift
+//  DesignAnInterface
+//
+//  Created by Kevin Wong on 10/15/24.
+//
+
+import SwiftUI
+
+@main
+struct DesignAnInterfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
